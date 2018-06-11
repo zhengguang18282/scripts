bb4534695790b18bdc5647d2afec5665cf841462
zhengguang666 <582635492@qq.com>
2018-06-11 16:41:03 +0800

    modify uboot config/kernel config/dts; 4220/3310 rm sgx

--- cfg_platform.sh
+++ cfg_platform.sh
@@ -2,6 +2,11 @@
 
 #config uboot
 CURR_PATH=$PWD
+if [ ! -f Makefile_bak ];then
+cp ${CURR_PATH}/Makefile ${CURR_PATH}/Makefile_bak
+fi
+cp ${CURR_PATH}/Makefile_bak ${CURR_PATH}/Makefile
+
 find ./board-support/u-boot-*/configs -name am*|grep -E "rom|rsb" |sed 's/^.*configs\///g' | grep -n '' | awk {'print "	" , $1'}
 echo ""
 read -p "Enter Number of rootfs Tarball: " CFGNUMBER
@@ -9,10 +14,7 @@ echo " "
 
 CFG_PLATNAME=`find ./board-support/u-boot-*/configs -name am*|grep -E "rom|rsb" |sed 's/^.*configs\///g' | grep -n '' | grep "${CFGNUMBER}:" | cut -c3- | awk {'print$1'}`
 CFG_UBOOT_MACHINE=${CFG_PLATNAME/defconfig/config}
-echo $CFG_PLATNAME
-echo $CFG_UBOOT_MACHINE
 ubootcfgline=`sed -n '/'"UBOOT_MACHINE"'/=' ./Rules.make`
-echo $ubootcfgline
 sed -i ''${ubootcfgline}'c 'UBOOT_MACHINE=$CFG_UBOOT_MACHINE'' ./Rules.make
 
 #config platform
@@ -20,8 +22,6 @@ CFG_PLAT=`echo $CFG_PLATNAME | cut -d \_ -f 1`
 CFG_BOARD=`echo $CFG_PLATNAME | cut -d \_ -f 2`
 CFG_PLATFORM=${CFG_PLAT}${CFG_BOARD}
 platcfgline=`sed -n '/'"PLATFORM"'/=' ./Rules.make`
-echo platcfgline=$platcfgline
-echo CFG_PLATFORM=$CFG_PLATFORM
 sed -i ''${platcfgline}'c 'PLATFORM=$CFG_PLATFORM'' ./Rules.make
 
 #config kernel
@@ -30,20 +30,23 @@ CFG_KERNEL_DEF=tisdk_${CFG_PLATFORM}_defconfig
 cp -f am57xx-adv_defconfig $CFG_KERNEL_DEF
 cd $CURR_PATH
 kdefline=`sed -n '/'"DEFCONFIG"'/=' ./Rules.make`
-echo kdefline=$kdefline
-echo CFG_KERNEL_DEF=$CFG_KERNEL_DEF
 sed -i ''${kdefline}'c 'DEFCONFIG=$CFG_KERNEL_DEF'' ./Rules.make
 
 #config dtb
 CFG_DTB_TARGET=${CFG_PLAT}-${CFG_BOARD}.dtb
-echo CFG_DTB_TARGET=$CFG_DTB_TARGET
 tempdtbline=`sed -n '/'"linux-dtbs:"'/=' ./Makefile`
 dtbline=`expr ${tempdtbline} + 5`
-echo tempdtbline=$tempdtbline
-echo dtbline=$dtbline
 dtbstring=`sed -n ''${dtbline}' p' ./Makefile`
 OLD_DTB_TARGET=`echo $dtbstring | cut -d \  -f 8`
-echo dtbstring=$dtbstring
-echo OLD_DTB_TARGET=$OLD_DTB_TARGET
 sed -i "s/${OLD_DTB_TARGET}/${CFG_DTB_TARGET}/g" ./Makefile
+
+#delete sgx
+if [ "${CFG_PLATFORM}" != "am335xrsb4221a1" ];then
+sgxline=`sed -n '/'"# ti-sgx-ddk-km module"'/=' ./Makefile`
+sgxend=`expr ${sgxline} + 24`
+sed -i ''${sgxline}','${sgxend}'d' ./Makefile
+sed -i "s/ ti-sgx-ddk-km / /g" ./Makefile
+sed -i "s/ ti-sgx-ddk-km_clean / /g" ./Makefile
+sed -i "s/ ti-sgx-ddk-km_install / /g" ./Makefile
+fi
 
